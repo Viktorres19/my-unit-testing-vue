7db68cf0c23edd67a7ce48eee930e726347c5523
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-11-21 14:01:18 +0200

    First unit test with Jest

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+<!--    output-->
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      loggedIn: false // input
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
